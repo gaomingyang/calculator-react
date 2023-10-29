e6bb0da8d30ce73428c1945d5224ba69bae8749a
Mingyang Gao <gaomingyangcn@gmail.com>
2023-10-29 15:55:42 -0600

    继续完善逻辑

--- note.md
+++ note.md
@@ -5,3 +5,6 @@ npm install node-sass --save
 
 需求
 https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-javascript-calculator
+
+就差User Story #13
+
--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import './App.scss';
 function App() {
   const [input,setInput] = useState("") //第一排显示的输入字符串
   const [result,setResult] = useState(0) //结果
+  const [currentNumber,setCurrentNumber] = useState("")
   const [currentNumberWithDecimal,setcurrentNumberWithDecimal] = useState(false) //最近的当前数字是否有小数点
   const [isFinish, setIsFinish] = useState(false)
 
   //点击数字
   function clickNumber(n) {
+    //没结束的时候
+    //不允许多个0开头。若前面已经有0，新输入还是0，则不执行
+    if (n === 0 && input === "0" ) {
+      return
+    }
+      
     //如果前面已经结束计算，再点击数字，这里重新设置输入字符串
     if (isFinish) {
       //忽略过去的input
@@ -18,13 +25,16 @@ function App() {
       setIsFinish(false) //修改结束状态为未结束
       return
     }
-      //没结束的时候
-      //不允许多个0开头。若前面已经有0，新输入还是0，则不执行
-      if (input === '0' && n === 0) {
-        return
-      }
-      let newInput = input +''+ n
-      setInput(newInput)
+    // console.log("oldinput:",input)  
+    // console.log("n:",n)
+    let newInput = input + String(n)
+
+    setInput(newInput)
+
+    //result显示当前一直在输入的数字
+    let newCurrentNumber = currentNumber + '' + n
+    setCurrentNumber(newCurrentNumber)
+    setResult(newCurrentNumber)
     
   }
 
@@ -32,20 +42,31 @@ function App() {
   function clickClear(){
     setInput("")
     setResult(0)
+    setCurrentNumber("")
+    setcurrentNumberWithDecimal(false)
+    setIsFinish(false)
   }
 
   // 点击小数点
   function clickDecimal() {
+    // console.log("click decimal")
     //若前面已经有了小数点，不能再点击小数点。
     if (currentNumberWithDecimal === false) {
+      let newInput 
       if (input.length === 0){
-        setInput("0.")
+        newInput="0."
       }else{
-        let newInput = input + '.'
-        setInput(newInput)
+        newInput = input + '.'
       }
-      
+      setInput(newInput)
+      setResult(newInput)
+      setCurrentNumber(newInput)
       setcurrentNumberWithDecimal(true)
+      
+      //小数点开头作为新一轮计算的情况，清楚上次的结束标记
+      if(isFinish) {
+        setIsFinish(false)
+      }
     }
   }
 
@@ -62,16 +83,22 @@ function App() {
       setInput(newInput)
     }
 
+    setCurrentNumber("") //点击运算符，上一个当前数字清空，避免影响result显示当前数字
     setcurrentNumberWithDecimal(false) //清除最近的数带小数点标记
   }
 
   //点击等号，计算结果
   function clickFinish() {
+    //避免按两次
+    if(isFinish){
+      return;
+    }
     try{
       let result = eval(input);
       setResult(result);
       let newinput = input + '='+ result
       setInput(newinput)
+      setCurrentNumber("")
       setcurrentNumberWithDecimal(false)  //清除最近的数带小数点标记
       setIsFinish(true) //标记上一个计算结束
     } catch(error) {
@@ -101,7 +128,6 @@ function App() {
           <div className="operateButton" id="one" onClick={()=>clickNumber(1)}>1</div>
           <div className="operateButton" id="two" onClick={()=>clickNumber(2)}>2</div>
           <div className="operateButton" id="three" onClick={()=>clickNumber(3)}>3</div>
-          
           <div className="operateButton" id="zero" onClick={()=>clickNumber(0)}>0</div>
           <div className="operateButton" id="decimal" onClick={clickDecimal}>.</div>
           <div className="operateButton" id="equals" onClick={clickFinish}>=</div>
