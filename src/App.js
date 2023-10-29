3fd5c18db7dca7d91d69394b56e509427cf341fb
Mingyang Gao <gaomingyangcn@gmail.com>
2023-10-29 15:07:57 -0600

    增加一些特殊逻辑

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import {useState} from 'react';
 import './App.scss';
 
 function App() {
-  const [input,setInput] = useState("")
-  const [result,setResult] = useState(0)
+  const [input,setInput] = useState("") //第一排显示的输入字符串
+  const [result,setResult] = useState(0) //结果
+  const [currentNumberWithDecimal,setcurrentNumberWithDecimal] = useState(false) //最近的当前数字是否有小数点
+  const [isFinish, setIsFinish] = useState(false)
 
   //点击数字
   function clickNumber(n) {
-    let newInput = input + n
-    setInput(newInput)
+    //如果前面已经结束计算，再点击数字，这里重新设置输入字符串
+    if (isFinish) {
+      //忽略过去的input
+      setInput(n) //显示输入
+      setResult(n) //显示输出结果
+      setIsFinish(false) //修改结束状态为未结束
+      return
+    }
+      //没结束的时候
+      //不允许多个0开头。若前面已经有0，新输入还是0，则不执行
+      if (input === '0' && n === 0) {
+        return
+      }
+      let newInput = input +''+ n
+      setInput(newInput)
+    
   }
 
   //点击清除
@@ -20,16 +36,33 @@ function App() {
 
   // 点击小数点
   function clickDecimal() {
-    let newInput = input + '.'
-    setInput(newInput)
+    //若前面已经有了小数点，不能再点击小数点。
+    if (currentNumberWithDecimal === false) {
+      if (input.length === 0){
+        setInput("0.")
+      }else{
+        let newInput = input + '.'
+        setInput(newInput)
+      }
+      
+      setcurrentNumberWithDecimal(true)
+    }
   }
 
   // 点击运算符
   function clickOperate(mark) {
-    console.log("clickOperate")
-    console.log(mark)
-    let newInput = input + mark
-    setInput(newInput)
+    if(isFinish) {
+      //若上一个运算刚结束，取其结果作为第一个输入。并且需要先清理之前的input
+      setInput(result)
+      let newInput = result + mark
+      setInput(newInput)
+      setIsFinish(false)
+    }else{
+      let newInput = input + mark
+      setInput(newInput)
+    }
+
+    setcurrentNumberWithDecimal(false) //清除最近的数带小数点标记
   }
 
   //点击等号，计算结果
@@ -39,6 +72,8 @@ function App() {
       setResult(result);
       let newinput = input + '='+ result
       setInput(newinput)
+      setcurrentNumberWithDecimal(false)  //清除最近的数带小数点标记
+      setIsFinish(true) //标记上一个计算结束
     } catch(error) {
       setResult('Error');
     }
